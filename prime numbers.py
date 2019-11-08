60491fad4c4db64eea32dfcc0e734e87342d9617
srrathi <57089237+srrathi@users.noreply.github.com>
2019-11-08 19:49:48 +0530

    Add files via upload

---
+++ prime numbers.py
@@ -0,0 +1,13 @@
+#Take the input from the user:   
+lower = 1  
+upper = int(input("Enter upper range: "))  #code to enter till which number we want to find prime no.'s'
+a= "abcd"
+for i in a:
+    print("*")
+for num in range(lower,upper + 1):  #loop to read all no.'s till the entered no.
+   if num > 1:   
+       for i in range(2,num): #loop to divide all the no.'s till the entered no. by the no.'s before it  
+           if (num % i) == 0: #if the remainder of the divison is 0 in any case when the no. is divided by the no.'s before it break this loop as it will not be a prime no. 
+               break  
+       else:  
+           print(num) #if the remainder is non-zero in all case  then print that no. as it will be a prime no.
