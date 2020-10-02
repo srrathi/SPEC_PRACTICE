6e71dd69c4a90840735d13a2997268cf56f0ad80
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2020-10-02 20:17:20 +0530

    Update prime numbers.py

--- prime numbers.py
+++ prime numbers.py
@@ -4,10 +4,10 @@ upper = int(input("Enter upper range: "))  #code to enter till which number we w
 a= "abcd"
 for i in a:
     print("*")
-for num in range(lower,upper + 1):  #loop to read all no.'s till the entered no.
-   if num > 1:   
-       for i in range(2,num): #loop to divide all the no.'s till the entered no. by the no.'s before it  
-           if (num % i) == 0: #if the remainder of the divison is 0 in any case when the no. is divided by the no.'s before it break this loop as it will not be a prime no. 
+for x in range(lower,upper + 1):  #loop to read all no.'s till the entered no.
+   if x > 1:   
+       for i in range(2,x): #loop to divide all the no.'s till the entered no. by the no.'s before it  
+           if (x % i) == 0: #if the remainder of the divison is 0 in any case when the no. is divided by the no.'s before it break this loop as it will not be a prime no. 
                break  
        else:  
-           print(num) #if the remainder is non-zero in all case  then print that no. as it will be a prime no.
+           print(x) #if the remainder is non-zero in all case  then print that no. as it will be a prime no.
